3aaa46178198edbe5f888bcda8c708a8a8ffd599
Aru_0077 <285309697@qq.com>
2025-08-02 21:11:28 +0800

    prisma-new

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,37 @@
+// src/prisma/prisma.service.ts
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Logger,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { AppConfigService } from '../config/config.service';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor(private configService: AppConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: configService.databaseUrl,
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connection established');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    this.logger.log('Database connection closed');
+  }
+}
